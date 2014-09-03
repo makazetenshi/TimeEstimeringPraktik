d55789db10f642c7ecdca6057b58a4524664768a
JaspurGaard <jaspurgaard@gmail.com>
2014-09-03 14:52:40 +0200

    Made changes to the database script and added test data

--- Praktik dbload.sql
+++ Praktik dbload.sql
@@ -78,7 +78,7 @@ StartDate datetime not null,
 EndDate datetime not null
 )
 
-CREATE TABLE Day
+CREATE TABLE DayTable
 (
 Id int IDENTITY(1,1) primary key,
 TypeName varchar(25) not null
@@ -88,8 +88,8 @@ CREATE TABLE DayActive
 (
 Id int IDENTITY(1,1) primary key,
 Period int foreign key references Period(Id),
-Day int foreign key references Day(Id),
-Number int,
+DayTable int foreign key references DayTable(Id),
+Number float,
 )
 
 CREATE TABLE Estimate
@@ -103,35 +103,36 @@ CREATE TABLE EstimateActive
 Id int IDENTITY(1,1) primary key,
 Period int foreign key references Period(Id),
 Estimate int foreign key references Estimate(Id),
-Number int
+Number float
 )
 
-CREATE TABLE Formulas
+CREATE TABLE Formula
 (
 Id int IDENTITY(1,1) primary key,
-TypeName varchar(25) not null
+Name varchar(25) not null,
+Formula varchar(50)
 )
 
 CREATE TABLE FormulasActive
 (
 Id int IDENTITY(1,1) primary key,
 Period int foreign key references Period(Id),
-Formulas int foreign key references Formulas(Id),
-Number int
+Formula int foreign key references Formula(Id),
+Number float
 )
 
-CREATE TABLE Formula
+CREATE TABLE Education
 (
 Id int IDENTITY(1,1) primary key,
-Formulas int foreign key references Formulas(Id),
-Name varchar(25) not null,
-Formula varchar(50)
+Name varchar(25)
 )
 
 CREATE TABLE Parameter
 (
 Id int IDENTITY(1,1) primary key,
-Parameter varchar(50) not null
+Name varchar(25) not null,
+Education int foreign key references Education(Id),
+Parameter float not null
 )
 
 CREATE TABLE FormulaParameter
@@ -142,3 +143,52 @@ Parameter int foreign key references Parameter(Id)
 )
 
 GO
+
+INSERT INTO Education values ('Datamatiker')
+INSERT INTO Education values ('IT Teknolog')
+INSERT INTO Education values ('Webudvikling')
+INSERT INTO Education values ('Multimediedesign')
+
+INSERT INTO Person values('Torben Krøjmand', 'fisk123', 'TK')
+INSERT INTO Person values('Søren Madsen', 'jens', 'SM')
+INSERT INTO Person values('Erik Jacobsen', 'kosteskab', 'EJ')
+
+INSERT INTO Period values(1, '20140801', '20141231')
+INSERT INTO Period values(2, '20140801', '20141231')
+INSERT INTO Period values(3, '20140801', '20141231')
+INSERT INTO Period values(2, '20140801', '20141231')
+
+INSERT INTO DayTable values('Vacation')
+INSERT INTO DayTable values('Holiday')
+INSERT INTO DayTable values('Illness')
+INSERT INTO DayTable values('Absent')
+
+INSERT INTO Estimate values('Booklist')
+INSERT INTO Estimate values('Other Estimates')
+
+INSERT INTO DayActive values(1, 1, 20)
+INSERT INTO DayActive values(1, 1, 6)
+INSERT INTO DayActive values(1, 1, 3)
+
+INSERT INTO EstimateActive values(1, 1, 9.2)
+INSERT INTO EstimateActive values(2, 1, 8)
+
+INSERT INTO Parameter values('uvtal', 1, 2.5)
+INSERT INTO Parameter values('hoptal', 1, 5)
+INSERT INTO Parameter values('divtal', 1, 0.75)
+INSERT INTO Parameter values('olctal', 1, 1.33)
+INSERT INTO Parameter values('praktiktal', 1, 0.66)
+
+INSERT INTO Formula values('uv', 'AntalLektioner*uvtal')
+INSERT INTO Formula values('hop', 'AntalGrupper*hoptal')
+INSERT INTO Formula values('div', 'AntalTimer*divtal')
+INSERT INTO Formula values('olc', 'AntalLektioner*olctal')
+INSERT INTO Formula values('praktik', 'AntalPersoner*praktiktal')
+
+INSERT INTO FormulaParameter values(1, 1)
+INSERT INTO FormulaParameter values(2, 2)
+INSERT INTO FormulaParameter values(3, 3)
+INSERT INTO FormulaParameter values(4, 4)
+INSERT INTO FormulaParameter values(5, 5)
+
+INSERT INTO FormulasActive values (1, 2, 100)
